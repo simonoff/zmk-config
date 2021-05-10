5b912fa7b99b79271e8979456642210aa31c74a4
Dennis Kempin <dennis.kempin@gmail.com>
2021-05-10 00:03:24 +0000

    Updated thumb cluster, added BT iayer

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-west build -d build/left -s zmk/app -b nice_nano -- -DSHIELD=corne_left -DZMK_CONFIG="$(pwd)/config"
-west build -d build/right -s zmk/app -b nice_nano -- -DSHIELD=corne_right -DZMK_CONFIG="$(pwd)/config"
+west build -d build/left -s zmk/app -b nice_nano -- -DSHIELD=corne_left -DZMK_CONFIG="$(pwd)/config" || exit
+west build -d build/right -s zmk/app -b nice_nano -- -DSHIELD=corne_right -DZMK_CONFIG="$(pwd)/config" || exit
+
+cp build/left/zephyr/zmk.uf2 build/left.uf2
+cp build/right/zephyr/zmk.uf2 build/right.uf2
